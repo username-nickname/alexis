f9d2915385a61dac26bb72633ce53b4d9d5f2844
username-nickname <86109747+username-nickname@users.noreply.github.com>
2021-06-18 15:23:38 +0300

    alexis
    alexis

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  });
+    
+});
